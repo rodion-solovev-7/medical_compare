a0a7e344ce17a2ec087f682bff437d5c3d808fd7
rodion-solovev-7 <rodion-solovev-7@users.noreply.github.com>
2023-01-22 14:59:00 +0500

    actualize analyze model + migrate

--- common/db/migrations/0000_init.sql
+++ common/db/migrations/0000_init.sql
@@ -1,4 +1,12 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE analysis (
-    id UUID PRIMARY KEY NOT NULL,
-    data VARCHAR(128) NULL
+    id UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
+    name VARCHAR(256) NOT NULL,
+    description TEXT NULL,
+    preparation TEXT NULL,
+    purpose TEXT NULL,
+    interpretation TEXT NULL,
+    article_number VARCHAR(64) NULL,
+    total_price DECIMAL(8,2) NULL,
+    add_data JSONB NOT NULL
 );

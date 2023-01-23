035111ad68e83712b4db24acfe9073aac01155e6
rodion-solovev-7 <rodion-solovev-7@users.noreply.github.com>
2023-01-23 22:44:05 +0500

    add migrations run on scraper start

--- scraper/pipelines.py
+++ scraper/pipelines.py
@@ -1,6 +1,3 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 
@@ -14,6 +11,10 @@ from scraper.items import InvitroAnalyzeItem
 
 
 class SaveDbPipeline:
+    @inject
+    def open_spider(self, _: Spider):
+        db.apply_migrations()
+
     @inject
     async def process_item(
         self,

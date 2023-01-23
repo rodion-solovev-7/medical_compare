5653ae9eddf64cba3e77c89e5bcece15cb207ec7
rodion-solovev-7 <rodion-solovev-7@users.noreply.github.com>
2023-01-23 23:05:49 +0500

    add cities get method

--- __entry_points__/server.py
+++ __entry_points__/server.py
@@ -9,7 +9,7 @@ from fastapi import FastAPI
 import config
 from common import db, di
 from web import health
-from web.v1 import analysis
+from web.v1 import analysis, city
 
 app = FastAPI()
 # без создания экземпляра контейнера di-инъекции работать не будут
@@ -31,6 +31,7 @@ async def shutdown_event():
 # add sub-apps
 app.mount('/health', health.app)
 app.mount('/v1/analysis', analysis.app)
+app.mount('/v1/city', city.app)
 # app.mount('/v1/jsonrpc', v1_jsonrpc.app)
 
 
